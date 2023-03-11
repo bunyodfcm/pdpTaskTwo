828210d0b99da4864589103dddc249a089d062b6
bunyodfcm <bunyod5877@gmail.com>
2023-03-11 18:07:06 +0500

    ss

--- src/companent/test/One.js
+++ src/companent/test/One.js
@@ -1,14 +1,27 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer, useState } from "react";
 import Two from "./Two";
 
 export const MyContext = createContext();
-
+function reducer(state, action) {
+  switch (action.type) {
+    case setColor:
+      return { ...state, color: "black" };
+    case setActive:
+      return { ...state, active: !state.active };
+    default:
+      return state;
+  }
+}
 const One = () => {
-    const [count, setCount] = useState(false)
+  // const [count, setCount] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    color: "white",
+    avtive: false,
+  });
   return (
-    <MyContext.Provider value={count} >
+    <MyContext.Provider value={count}>
       <div>
-        <Two/>
+        <Two />
       </div>
     </MyContext.Provider>
   );
